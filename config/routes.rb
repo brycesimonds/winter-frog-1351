277aba65884e2d14329407677c6f0021aa62ee8f
Bryce Simonds <bsimonds14@gmail.com>
2022-08-08 10:01:54 -0600

    Added route for delete plant plot

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :plots, only: [:index]
+
+  delete '/plant_plots/:plant_id/:plot_id', to: 'plant_plots#destroy'
 end
