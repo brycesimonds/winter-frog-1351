afc4fba8a8b08273e32047301c131554b2bf8f2a
Bryce Simonds <bsimonds14@gmail.com>
2022-08-08 11:25:31 -0600

    Refactor: added relationships, refactored distinct_plants method

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,7 +1,16 @@
 class Garden < ApplicationRecord
   has_many :plots
+  has_many :plant_plots, through: :plots
+  has_many :plants, through: :plant_plots
 
   def distinct_plants
-    plots.joins(:plants).select('plants.*').where('days_to_harvest < ?', '100').distinct
+    plants.select('plants.*')
+    .where('days_to_harvest < ?', '100')
+    .distinct
   end
+
+  # def distinct_plants
+  #   plots.joins(:plants).select('plants.*').where('days_to_harvest < ?', '100').distinct
+  # end
+  #^This above method was created before creating additional relationships and worked to pass the User Story 3. I did not use it because I could not get the appropriate test to pass for it. 
 end
--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -4,4 +4,35 @@ RSpec.describe Garden do
   describe 'relationships' do
     it { should have_many(:plots) }
   end
+
+  describe 'instance methods' do
+    describe '.distinct_plants' do
+      it 'returns unqiue plants for the given garden' do
+        garden_1 = Garden.create!(name: 'Fun Garden', organic: true)
+
+        plot_1 = Plot.create!(number: 20, size: 'Large', direction: 'East', garden_id: garden_1.id)
+        plot_2 = Plot.create!(number: 30, size: 'Medium', direction: 'West', garden_id: garden_1.id)
+        plot_3 = Plot.create!(number: 40, size: 'Small', direction: 'North', garden_id: garden_1.id)
+        plot_4 = Plot.create!(number: 50, size: 'Large', direction: 'South', garden_id: garden_1.id)
+    
+        plant_1 = Plant.create!(name: 'Rose', description: 'Thorny', days_to_harvest: 100)
+        plant_2 = Plant.create!(name: 'Tulip', description: 'Yellow', days_to_harvest: 101)
+        plant_3 = Plant.create!(name: 'Cactus', description: 'Green', days_to_harvest: 99)
+        plant_4 = Plant.create!(name: 'Succulent', description: 'Never needs water sometimes', days_to_harvest: 1)
+    
+        PlantPlot.create!(plant_id: plant_1.id, plot_id: plot_1.id)
+        PlantPlot.create!(plant_id: plant_2.id, plot_id: plot_1.id)
+        PlantPlot.create!(plant_id: plant_2.id, plot_id: plot_2.id)
+        PlantPlot.create!(plant_id: plant_3.id, plot_id: plot_1.id)
+        PlantPlot.create!(plant_id: plant_3.id, plot_id: plot_2.id)
+        PlantPlot.create!(plant_id: plant_3.id, plot_id: plot_3.id)
+        PlantPlot.create!(plant_id: plant_4.id, plot_id: plot_1.id)
+        PlantPlot.create!(plant_id: plant_4.id, plot_id: plot_2.id)
+        PlantPlot.create!(plant_id: plant_4.id, plot_id: plot_3.id)
+        PlantPlot.create!(plant_id: plant_4.id, plot_id: plot_4.id)
+        
+        expect(garden_1.distinct_plants).to match_array([plant_3, plant_4])
+      end
+    end
+  end
 end
