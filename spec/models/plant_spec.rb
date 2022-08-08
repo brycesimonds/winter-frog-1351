04e84e4185347f33f6e31c91e0d8c1881957e460
Bryce Simonds <bsimonds14@gmail.com>
2022-08-08 09:19:18 -0600

    Relationship set up complete

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Plant do
   describe 'relationships' do
-    it { should have_many(:plots) }
     it { should have_many(:plant_plots) }
+    it { should have_many(:plots).through(:plant_plots) }
   end
 end
